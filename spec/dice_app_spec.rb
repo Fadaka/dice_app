05ea153ae507fa65a2c3eec22c2c95c2528442db
Fadaka <master_kunle14@hotmail.co.uk>
2021-11-17 14:42:39 +0000

    added failing test to check a number of simultaneous dice games can be played

--- spec/dice_app_spec.rb
+++ spec/dice_app_spec.rb
@@ -9,4 +9,7 @@ describe Dice do
         magic_dice = Dice.new
         expect(magic_dice.roll).to be_between(1,6).inclusive
     end
+    it 'able to roll multiple dice simultaneously' do
+    expect(magic_dice.dice_played.length).to be_eq(:dice_rolls)
+    end
 end 
